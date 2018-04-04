14a8e461cd680bb45700285b838dc5a9ff0a2000
Rumpu-Jussi <kontza@gmail.com>
2018-04-04 20:39:17 +0300

    Getter-script added.

---
+++ get-latest.fish
@@ -0,0 +1,9 @@
+set -g XVM_VERSION (curl -s http://nightly.modxvm.com/download/default/xvm_version.txt|head -1)
+set -g XVM_RELEASE "xvm-$XVM_VERSION.zip"
+rm -rf latest
+echo "Downloading '$XVM_RELEASE'..."
+wget -q https://dl1.modxvm.com/bin/$XVM_RELEASE -O $XVM_RELEASE
+echo "Extracting..."
+unzip -q $XVM_RELEASE -d latest
+echo "Done."
+bcomp "WoT configs"
