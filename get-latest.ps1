2ba459b1f07ad4da80bf59f7d2028d3b9d28a6a4
Rumpu-Jussi <kontza@gmail.com>
2018-06-04 20:58:33 +0300

    Script updated.

--- get-latest.ps1
+++ get-latest.ps1
@@ -1,18 +1,32 @@
 param(
-    [string]$XVM_VERSION = $(iwr -uri http://nightly.modxvm.com/download/default/xvm_version.txt -usebasic).content.trim()
+    [string]$XVM_VERSION = ""
 )
-$XVM_RELEASE = "xvm-$XVM_VERSION.zip"
-if (test-path "latest") {
-    rm -recurse -force latest
+
+if($XVM_VERSION) {
+    Write-Output "Trying to work with XVM $XVM_VERSION..."
+} else {
+    $answer = Read-Host 'No XVM version specified (via -x). Try with the latest (yes/no)'
+    if ($answer.Trim().ToLower().StartsWith("y")) {
+        $XVM_VERSION = $(Invoke-WebRequest -uri http://nightly.modxvm.com/download/default/xvm_version.txt -usebasic).content.Trim()
+    } else {
+        Write-Output "Aborting, cannot continue."
+    }
 }
-echo "Downloading '$XVM_RELEASE'..."
-iwr -uri "https://dl1.modxvm.com/bin/$XVM_RELEASE" -out "$XVM_RELEASE"
-echo "Extracting..."
-Expand-Archive "$XVM_RELEASE" -DestinationPath latest
-echo "Clearing old PYC-files..."
-pushd ..\..
-get-childitem . -include *.pyc -recurse | foreach ($_) {remove-item $_.fullname}
-popd
-echo "Launch Beyond Compare..."
-bcomp bcomp . .\latest\res_mods\configs
-echo "Done."
+
+if($XVM_VERSION) {
+    $XVM_RELEASE = "xvm-$XVM_VERSION.zip"
+    if (test-path "latest") {
+        Remove-Item -recurse -force latest
+    }
+    Write-Output "Downloading '$XVM_RELEASE'..."
+    Invoke-WebRequest -uri "https://dl1.modxvm.com/bin/$XVM_RELEASE" -out "$XVM_RELEASE"
+    Write-Output "Extracting..."
+    Expand-Archive "$XVM_RELEASE" -DestinationPath latest
+    Write-Output "Clearing old PYC-files..."
+    Push-Location ..\..
+    get-childitem . -include *.pyc -recurse | foreach ($_) {remove-item $_.fullname}
+    Pop-Location
+    Write-Output "Launch Beyond Compare..."
+    bcomp . .\latest\res_mods\configs
+    Write-Output "Done."
+}
