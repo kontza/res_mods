12fd8e5cb480da9324be901c1e6e04aa8f9f1d9d
Rumpu-Jussi <kontza@gmail.com>
2018-08-09 22:19:13 +0300

    finalize.ps1, to no avail

--- finalize.ps1
+++ finalize.ps1
@@ -9,7 +9,7 @@ if ($verbose) {
 
 $prefix = "Does not exist:"
 
-if (Test-Path $scriptDir/latest/res_mods/configs/xvm/py_macro) {
+if (Test-Path "$scriptDir/latest/res_mods/configs/xvm/py_macro") {
     Remove-Item -r "$scriptDir/xvm/py_macro"
     Move-Item "$scriptDir/latest/res_mods/configs/xvm/py_macro" "$scriptDir/xvm/"
     Write-Output "Processed 'py_macro'."
@@ -20,7 +20,7 @@ if (Test-Path $scriptDir/latest/res_mods/configs/xvm/py_macro) {
 Copy-Item resources/sixthsense.png "$scriptDir/../mods/shared_resources/xvm/res/sixthsense.png"
 Write-Output "Copied the sixth sense -image."
 
-if (Test-Path $scriptDir/latest/mods) {
+if (Test-Path "$scriptDir/latest/mods") {
     Remove-Item -r "$scriptDir/../../mods"
     Move-Item "$scriptDir/latest/mods" "$scriptDir/../.."
     Write-Output "Processed 'mods'."
