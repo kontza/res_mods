815ce3d057480e0960a41d38e6ac7421137dc112
Rumpu-Jussi <kontza@gmail.com>
2020-10-04 15:56:30 +0300

    8.6.4/1.10.0.4

--- install.ps1
+++ install.ps1
@@ -30,7 +30,7 @@
     Author: Juha Ruotsalainen
 #>
 param (
-    [string]$XVM_VERSION = "8.6.3",
+    [string]$XVM_VERSION = "8.6.4",
     [switch]$BCompare = $false,
     [switch]$sixthSense,
     [switch]$finalize
